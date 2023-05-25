44ca264b9bd0cff1e63d328217c063f4777d7c24
cakihorse <91017164+cakihorse@users.noreply.github.com>
2023-05-25 09:45:59 +0200

    Correction de bug

--- main.ts
+++ main.ts
@@ -1,20 +1,16 @@
-let rang = 0
-rang = 0
-
 input.onButtonPressed(Button.A, function () {
     rang += 1
-    if (rang > 6){
-        rang += 1
+    if (rang > 6) {
+        rang = 1
     }
-    
 })
-
-
+let rang = 0
+rang = 0
 basic.forever(function () {
     if (rang == 1) {
         basic.showString("t")
     }
-    if (rang== 2) {
+    if (rang == 2) {
         basic.showString("e")
     }
     if (rang == 3) {
@@ -29,5 +25,4 @@ basic.forever(function () {
     if (rang == 6) {
         basic.showString("o")
     }
-    
 })
